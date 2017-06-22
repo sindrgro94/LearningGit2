15cf2ffccaf93aa0da39401247b303c65932a903
sgrostad <sgrostad@yahoo-inc.com>
2017-06-22 08:57:40 +0200

    adding evaluate with int

--- src/main/java/Sindre/Calculator.java
+++ src/main/java/Sindre/Calculator.java
@@ -4,10 +4,13 @@ package Sindre;
  * Created by sgrostad on 21/06/2017.
  */
 public class Calculator {
-    public int evaluate(String expression) {
+    public static int evaluate(String expression) {
         int sum = 0;
         for (String summand: expression.split("\\+"))
             sum += Integer.valueOf(summand);
         return sum;
+    }
+    public static int evaluateWithInt(int x, int y){
+        return x+y;
     }
 }
