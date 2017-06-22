720202f54a532a113eedf7a9fa178f101c3f0787
sgrostad <sgrostad@yahoo-inc.com>
2017-06-22 09:12:39 +0200

    typo in main

--- src/main/java/Sindre/App.java
+++ src/main/java/Sindre/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main( String[] args )
     {
         int answer = Calculator.evaluate("1+2");
-        int sameAnswer = Calculator.evaluateInt(1,2);
+        int sameAnswer = Calculator.evaluateWithInt(1,2);
     }
 }
